855b0e74562deded9f1980f7e6f60c7568a9e15a
Nitish Shah <nitishpshah@gmail.com>
2019-04-13 12:39:54 +0530

    added open_terminals.bat

---
+++ open_terminals.bat
@@ -0,0 +1,3 @@
+start powershell.exe -noexit -command "cd D:\_my_programs_unsynced\expos\myexpos\xfs-interface;bash;"
+start powershell.exe -noexit -command "cd D:\_my_programs_unsynced\expos\myexpos\spl;bash"
+start powershell.exe -noexit -command "cd D:\_my_programs_unsynced\expos\myexpos\xsm;bash"
